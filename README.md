55c72bb7eb0298f77234b04e29e6bdde9f57cef1
Leonardo Merza <ljmerza@gmail.com>
2020-01-17 21:46:33 -0500

    automated commit 17/01/2020 21:46:33

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # Syncarr
-Syncs two Radarr/Sonarr/Lidarr servers through the web API.
+Syncs two Radarr/Sonarr/Lidarr servers through the web API. Useful for syncing a 4k radarr/sonarr instance to a 1080p radarr/sonarr instance.
 
-### Configuration
+
+## Configuration
  1. Edit the config.conf file and enter your servers URLs and API keys for each server.  
- 2. Add the profile id and movie path for the radarr instance the movies will be synced to
+ 2. Add the profile name (case insensitive) and movie path for the radarr instance the movies will be synced to:
     ```ini
     [radarrA]
     url = https://example.com:443
     key = XXXXX
+    profile = 4k # optional - will only sync movies with this profile (without this will sync ALL movies from A to B)
+    path = /data/Movies # optional - required if sync_bidirectionally = 1 in general settings
     
     [radarrB]
     url = http://127.0.0.1:8080
     key = XXXXX
-    profile_id = 1
+    profile = 1080p
     path = /data/4k_Movies
     ```
  3. Or if you want to sync two sonarr instances:
@@ -24,7 +27,7 @@ Syncs two Radarr/Sonarr/Lidarr servers through the web API.
     [sonarrB]
     url = http://127.0.0.1:8080
     key = XXXXX
-    profile_id = 1
+    profile = 1080p
     path = /data/4k_Shows
  
  4. Or if you want to sync two lidarr instances:
@@ -36,29 +39,39 @@ Syncs two Radarr/Sonarr/Lidarr servers through the web API.
     [lidarrB]
     url = http://127.0.0.1:8080
     key = FCKGW-RHQQ2-YXRKT-8TG6W-2B7Q8
-    profile_id = 1
+    profile = lossless
     path = /data/lossless_music
     ```
-
-    **Note**: you cannot have a mix of radarr, lidarr, or sonarr config setups at the same time.
 
  5. By default Syncarr will sync unidirectionally from instance A to instance B but you can add bidirectional syncing with:
      ```ini
      [general]
      sync_bidirectionally = 1
      ```
+ 6. syncarr will try to find the `profile_id` given a profile name, if njo match is found, syncarr will exit with error. You can also specify a `profile_id` directly isntead of a profile:
+     ```ini
+    [radarrB]
+    url = http://127.0.0.1:8080
+    key = XXXXX
+    profile_id = 1
+    path = /data/4k_Movies
+    ```
+---
 
-To get the `profile_id`, open developer tools (f12) in the browser and go to network tab and reload your *arr instance. There will be a profile network call and under the preview tab, you'll see a list of your profiles you have setup. In my example, I only have three profiles (HD-1080p, 4K, and HD). You need the `id` of what you want your instance B movies to be. Example: If you are syncing 4k radarr to 1080p radarr then get you 1080p profile id from the 1080p radarr instance (1080p is instance B in this example - sync FROM A TO B)
+## Notes
+* you cannot have a mix of radarr, lidarr, or sonarr config setups at the same time.
+* for radarr, sonarr, and lidarr, an optional `profile` can be added to instance A so only content with that `profile` will be synced from instance A to instance B. This will be also true if bidirectional syncing is enabled **only** if both `profile`s are supplied. The same behavior can be spplied with `profile_id`s.
 
-<img src='./profile_id_example.png'/>
 
-#### How to Run
+---
+## How to Run
 You can run this script directly or through a Cron:
 ```bash
 python index.py
 ```
 
-#### Docker Compose
+---
+## Docker Compose
 This script can run through a docker container with a set interval (default every 5 minutes)
 
 ```
@@ -71,7 +84,7 @@ syncarr:
         RADARR_A_KEY: XXXXX
         RADARR_B_URL: http://127.0.0.1:8080
         RADARR_B_KEY: XXXXX
-        RADARR_B_PROFILE_ID: 1
+        RADARR_B_PROFILE: 1080p
         RADARR_B_PATH: /data/4k_Movies
         SYNC_INTERVAL_SECONDS: 300
 ```
@@ -88,7 +101,7 @@ syncarr:
         SONARR_A_KEY: XXXXX
         SONARR_B_URL: http://127.0.0.1:8080
         SONARR_B_KEY: XXXXX
-        SONARR_B_PROFILE_ID: 1
+        SONARR_B_PROFILE: 1080p
         SONARR_B_PATH: /data/4k_Movies
         SYNC_INTERVAL_SECONDS: 300
 ```
@@ -105,24 +118,29 @@ syncarr:
         LIDARR_A_KEY: FCKGW-RHQQ2-YXRKT-8TG6W-2B7Q8
         LIDARR_B_URL: http://127.0.0.1:8080
         LIDARR_B_KEY: FCKGW-RHQQ2-YXRKT-8TG6W-2B7Q8
-        LIDARR_B_PROFILE_ID: 1
+        LIDARR_B_PROFILE: 1080p
         LIDARR_B_PATH: /data/4k_Movies
         SYNC_INTERVAL_SECONDS: 300
 ```
 
-#### Docker
+---
+## Docker
 For just plain docker:
 
 ```
-docker run -it --rm --name syncarr -e RADARR_A_URL=https://example.com:443 -e RADARR_A_KEY=XXXXX -e RADARR_B_URL=http://127.0.0.1:8080 -e RADARR_B_KEY=XXXXX -e RADARR_B_PROFILE_ID=1 -e RADARR_B_PATH=/data/4k_Movies -e SYNC_INTERVAL_SECONDS=300 syncarr/syncarr
+docker run -it --rm --name syncarr -e RADARR_A_URL=https://example.com:443 -e RADARR_A_KEY=XXXXX -e RADARR_B_URL=http://127.0.0.1:8080 -e RADARR_B_KEY=XXXXX -e RADARR_B_PROFILE=1080p -e RADARR_B_PATH=/data/4k_Movies -e SYNC_INTERVAL_SECONDS=300 syncarr/syncarr
 ```
 
-#### Requirements
+**Note:** You can also specify the `PROFILE_ID` directly through the `*ARR_A_PROFILE_ID` and `*ARR_B_PROFILE_ID` ENV variables.
+
+---
+## Requirements
  * Python 3.4 or greater
  * 2x Radarr/Sonarr/Lidarr servers
  * Install requirements.txt
-
-#### Debugging
+ * 
+---
+## Debugging
 If you need to debug syncarr then you can either set the log level through the config file:
 
 ```ini
@@ -130,8 +148,8 @@ If you need to debug syncarr then you can either set the log level through the c
 log_level = 10
 ```
     
-Or in docker, set the `LOG_LEVEL` ENV var. Default is set to `20` (info only) but you can set to `10` to get debug info as well. When pasting debug logs online, **make sure to remove any apikeys and any other data you don't want others to see.**
+Or in docker, set the `LOG_LEVEL` ENV variable. Default is set to `20` (info only) but you can set to `10` to get debug info as well. When pasting debug logs online, **make sure to remove any apikeys and any other data you don't want others to see.**
 
-
-#### Disclaimer
+---
+## Disclaimer
 Back up your instances before trying this out. I am not responsible for any lost data.
--- syncarr/config.py
+++ syncarr/config.py
@@ -9,7 +9,7 @@ import sys
 import time
 
 DEV = os.environ.get('DEV', False)
-VER = '1.2.0'
+VER = '1.3.0'
 
 def ConfigSectionMap(section):
     '''get all config options from config file'''
@@ -74,6 +74,10 @@ consoleHandler.setFormatter(logFormatter)
 logger.addHandler(consoleHandler)
 
 logger.debug('Syncarr Version {}'.format(VER))
+logger.info('log level {}'.format(log_level))
+
+if DEV:
+    logger.info('-----------------DEVV-----------------')
 ########################################################################################################################
 
 
@@ -121,45 +125,67 @@ sync_bidirectionally = get_config_value('SYNCARR_BIDIRECTIONAL_SYNC', 'bidirecti
 if sync_bidirectionally:
     sync_bidirectionally = int(sync_bidirectionally) or 0
 
+
 ########################################################################################################################
-# find if we are syncing radarr, lidarr, or sonarr
+# make sure we have radarr, lidarr, OR sonarr
+if (
+    (sonarrA_url and radarrA_url) or 
+    (sonarrA_url and radarrB_url) or
+    (sonarrA_url and lidarrA_url) or
+    (sonarrA_url and lidarrB_url) or
+
+    (radarrA_url and lidarrA_url) or
+    (radarrA_url and lidarrB_url) or
+    (radarrB_url and lidarrA_url) or
+    (radarrB_url and lidarrB_url)
+):
+    logger.error('cannot have more than one *arr type profile(s) setup at the same time')
+    sys.exit(0)
+
+########################################################################################################################
+# get generic instanceA/B variables
 instanceA_url = ''
 instanceA_key = ''
+instanceA_profile = ''
+instanceA_profile_id = ''
+
 instanceB_url = ''
 instanceB_key = ''
-instanceA_profile_id = ''
+instanceB_profile = ''
 instanceB_profile_id = ''
 instanceB_path = ''
-api_content_path = ''
-api_search_path = ''
+
+api_content_path = '' # url path to add content
+api_search_path = '' # url path to search for content on RSS feeds
+api_profile_path = '' # url path to get quality profiles
 
 is_radarr = False
 is_sonarr = False
 is_lidarr = False
+
+content_id_key = '' # the unique id for a content item
+
 instanceA_is_v3 = False
 instanceB_is_v3 = False
 
 
 if radarrA_url and radarrB_url:
-    assert radarrA_url
-    assert radarrA_key
-
-    assert radarrB_url
-    assert radarrB_key
-    assert radarrB_profile_id
-    assert radarrB_path
-
     instanceA_url = radarrA_url
     instanceA_key = radarrA_key
+    instanceA_profile = radarrA_profile
     instanceA_profile_id = radarrA_profile_id
+    instanceA_path = radarrA_path
     
     instanceB_url = radarrB_url
     instanceB_key = radarrB_key
+    instanceB_profile = radarrB_profile
     instanceB_profile_id = radarrB_profile_id
     instanceB_path = radarrB_path
 
     api_content_path = 'api/movie'
     api_search_path = 'api/command'
+    api_profile_path = 'api/profile'
+
     content_id_key = 'tmdbId'
 
     is_radarr = True
@@ -167,25 +193,23 @@ if radarrA_url and radarrB_url:
     instanceB_is_v3 = False if radarrB_is_version_3 else True
 
 elif lidarrA_url and lidarrB_url:
-    assert lidarrA_url
-    assert lidarrA_key
-
-    assert lidarrB_url
-    assert lidarrB_key
-    assert lidarrB_profile_id
-    assert lidarrB_path
-
     instanceA_url = lidarrA_url
     instanceA_key = lidarrA_key
-    instanceA_profile_id = lidarrB_profile_id
+    instanceA_profile = lidarrA_profile
+    instanceA_profile_id = lidarrA_profile_id
+    instanceA_path = lidarrA_path
+
 
     instanceB_url = lidarrB_url
     instanceB_key = lidarrB_key
+    instanceB_profile = lidarrB_profile
     instanceB_profile_id = lidarrB_profile_id
     instanceB_path = lidarrB_path
 
     api_content_path = 'api/v1/artist'
     api_search_path = 'api/v1/command'
+    api_profile_path = 'api/v1/qualityprofile'
+
     content_id_key = 'foreignArtistId'
 
     is_lidarr = True
@@ -193,25 +217,22 @@ elif lidarrA_url and lidarrB_url:
     instanceB_is_v3 = True
 
 elif sonarrA_url and sonarrB_url:
-    assert sonarrA_url
-    assert sonarrA_key
-
-    assert sonarrB_url
-    assert sonarrB_key
-    assert sonarrB_profile_id
-    assert sonarrB_path
-
     instanceA_url = sonarrA_url
     instanceA_key = sonarrA_key
+    instanceA_profile = sonarrA_profile
     instanceA_profile_id = sonarrA_profile_id
+    instanceA_path = sonarrA_path
 
     instanceB_url = sonarrB_url
     instanceB_key = sonarrB_key
+    instanceB_profile = sonarrB_profile
     instanceB_profile_id = sonarrB_profile_id
     instanceB_path = sonarrB_path
 
     api_content_path = 'api/v3/series'
     api_search_path = 'api/command'
+    api_profile_path = 'api/v3/qualityprofile'
+
     content_id_key = 'tvdbId'
 
     is_sonarr = True
@@ -223,10 +244,9 @@ elif sonarrA_url and sonarrB_url:
 logger.debug({
     'instanceA_url': instanceA_url,
     'instanceA_key': instanceA_key,
+    'instanceB_path': instanceB_path,
     'instanceB_url': instanceB_url,
     'instanceB_key': instanceB_key,
-    'instanceA_profile_id': instanceA_profile_id,
-    'instanceB_profile_id': instanceB_profile_id,
     'instanceB_path': instanceB_path,
     'api_content_path': api_content_path,
     'api_search_path': api_search_path,
@@ -245,18 +265,14 @@ assert api_content_path
 assert api_search_path
 assert content_id_key
 
-########################################################################################################################
-# make sure we have radarr OR sonarr
-if (
-    (sonarrA_url and radarrA_url) or 
-    (sonarrA_url and radarrB_url) or
-    (sonarrA_url and lidarrA_url) or
-    (sonarrA_url and lidarrB_url) or
+# if two way sync need instance A path and profile
+if sync_bidirectionally:
+    assert instanceA_path
+    if not instanceB_profile_id and not instanceB_profile:
+        logger.error('profile_id or profile is required for *arr instance A if bidirectionalv is enabled')
+        sys.exit(0)
 
-    (radarrA_url and lidarrA_url) or
-    (radarrA_url and lidarrB_url) or
-    (radarrB_url and lidarrA_url) or
-    (radarrB_url and lidarrB_url)
-):
-    logger.error('cannot have more than one *arr type profile(s) setup at the same time')
+
+if not instanceB_profile_id and not instanceB_profile:
+    logger.error('profile_id or profile is required for *arr instance B')
     sys.exit(0)
